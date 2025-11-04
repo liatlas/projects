146aa159439bfa3fce025a93c7ad8e4e289ea9dd
liatlas <atlasli2016@gmail.com>
2025-11-04 16:57:58 -0500

    rewrite the source tutorial with a class-based implementation

--- quote-guessing-game/main.py
+++ quote-guessing-game/main.py
@@ -1,81 +1,104 @@
-import requests # web requests
-from bs4 import BeautifulSoup # processing html and xhtml
+import requests
+from bs4 import BeautifulSoup
 from csv import writer
 from time import sleep
 from random import choice
 
-# a list that will store all scraped data
-all_quotes = []
+class GuessingGame:
+    def __init__(self, base_url, url, guesses):
+        self._base_url = base_url
+        self._url = url
+        self._all_quotes = []
+        self._remaining_guesses = guesses
 
-base_url = "http://quotes.toscrape.com/"
+    def make_requests(self):
+        while self._url:
+            result = requests.get(f"{self._base_url}{self._url}")
+            print(f"Now scraping {self._base_url}{self._url}")
+            soup = BeautifulSoup(result.text, "html.parser")
 
-# part of the page that will change
-url = "/page/1"
+            self.quotes = soup.find_all(class_="quote")
 
-while url:
-    
-    # concatenating urls
-    # making request
+            for quote in self.quotes:
+                self._all_quotes.append({
+                        "text": quote.find(class_="text").get_text(),
+                        "author": quote.find(class_="author").get_text(),
+                        "bio-link": quote.find("a")["href"]
+                    })
+            next_btn = quote.find(class_="next")
+            self._url = next_bin.find("a")["href"] if next_btn else None
+            sleep(2)
 
-    result = requests.get(f"{base_url}{url}")
-    print(f"Now scraping {base_url}{url}")
-    soup = BeautifulSoup(result.text, "html.parser")
+    def play(self):
+        self.quote = choice(self._all_quotes)
+        self.print_quote()
+        self.guess = ''
+        
+        while self.incorrect_guess() and self._remaining_guesses > 0:
+            self.get_guess()
 
-    # extracting all elements
-    quotes = soup.find_all(class_="quote")
-    
-    for quote in quotes:
-        all_quotes.append({
-            "text": quote.find(class_="text").get_text(),
-            "author": quote.find(class_="author").get_text(),
-            "bio-link": quote.find("a")["href"]
-        })
-    next_btn = quote.find(class_="next")
-    url = next_bin.find("a")["href"] if next_btn else None
-    sleep(2)
 
-quote = choice(all_quotes)
-remaining_guesses = 4
-print("Here's a quote: ")
-print(quote["text"])
+            if not self.incorrect_guess():
+                print("You got it right!")
+                break
 
-guess = ''
+            self._remaining_guesses -= 1
 
-while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
-    guess = input(
-            f"Who said this quote? Guesses remaining {remaining_guesses}: "
-            )
-    if guess.lower() == quote['author'].lower():
-        print("You got it right!")
-        break
-    remaining_guesses -= 1
+            hints = {
+            3: self.hint_birth_place_date,
+            2: self.hint_first_name,
+            1: self.hint_last_name
+            }
 
-    if remaining_guesses == 3:
-        res = requests.get(f"{base_url}{quote['bio-link']}")
-        soup = BeautifulSoup(res.text, "html.parser")
+            hint_func = hints.get(self._remaining_guesses)
+
+            if hint_func:
+                hint_func()
+            else:
+                print("Sorry, You're out of guesses")
+                print(f"The author was {self.quote['author']}")
+
+    def get_guess(self):
+        self.guess = input(
+                f"Who said this quote? Guesses remaining {self._remaining_guesses}: "
+                )
+
+    def hint_birth_place_date(self):
+        result = requests.get(f"{self._base_url}{self.quote['bio-link']}")
+        soup = BeautifulSoup(result.text, "html.parser")
         birth_date = soup.find(class_="author-born-date").get_text()
         birth_place = soup.find(class_="author-born-location").get_text()
-        print(
-                f"Here's a hint: The author was born on {birth_date}{birth_place}")
-    elif remaining_guesses == 2:
-        print(
-            f"Here's a hint: The author's first name starts with: {quote['author'][0]}")
+        print(f"Here's a hint: The author was born on {birth_date} {birth_place}")
     
-    elif remaining_guesses == 1:
-        last_initial = quote["author"].split(" ")[1][0]
-        print(
-            f"Here's a hint: The author's last name starts with: {last_initial}")
-    
-    else:
-        print(
-            f"Sorry, you ran out of guesses. The answer was {quote['author']}")
+    def hint_first_name(self):
+        print(f"Here's a hint: The author's first name starts with: {self.quote['author'][0]}")
 
-    
+    def hint_last_name(self):
+        last_initial = self.quote["author"].split(" ")[1][0]
+        print(f"Here's a hint: The author's first name starts with: {last_initial}")
+
+    def incorrect_guess(self):
+        if self.guess.lower() != self.quote["author"].lower():
+            return True
+        return False
+
+    def print_quote(self):
+        print("Here's a quote: ")
+        print(self.quote["text"])
+
+
 
 def main():
-    all_quotes = []
-    base_url = 'https://quotes.toscrape.com/'
-    url = '/page/1'
+    base_url = "https://quotes.toscrape.com/"
+    url = "/page/1"
+    guesses = 4
+
+
+    g = GuessingGame(base_url, url, guesses)
+    
+    g.make_requests()
+
+    g.play()
 
 if __name__ == "__main__":
     main()
